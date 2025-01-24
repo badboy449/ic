667f851efe2a3620cb1d85ca5aaf9ae38de6d697
Max Summe <maximilian.summe@dfinity.org>
2025-01-23 16:59:46 -0800

    Clippy

--- rs/nns/governance/src/governance/benches.rs
+++ rs/nns/governance/src/governance/benches.rs
@@ -550,8 +550,8 @@ fn list_neurons_by_subaccount_benchmark() -> BenchResult {
         .collect::<BTreeMap<u64, NeuronProto>>();
 
     let subaccounts = neurons
-        .iter()
-        .map(|(_, neuron)| NeuronSubaccount {
+        .values()
+        .map(|neuron| NeuronSubaccount {
             subaccount: neuron.account.clone(),
         })
         .collect();
